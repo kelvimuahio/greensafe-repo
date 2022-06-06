1a345d417baab8734b318e62bbdb500a8134206b
Kelvi Muahio <kelvi.muahio@gmail.com>
2022-06-06 06:48:47 +0200

    first push

---
+++ src/test/java/com/consulting/demo/GreensafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consulting.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreensafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
